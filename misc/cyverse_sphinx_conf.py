e91e6569381bdec2aa7c0b2ac887d986ed517d98
Tyson L. Swetnam <tyson-swetnam@users.noreply.github.com>
2021-10-28 13:00:29 -0700

    Update cyverse_sphinx_conf.py

--- misc/cyverse_sphinx_conf.py
+++ misc/cyverse_sphinx_conf.py
@@ -71,7 +71,7 @@ def setup(app):
         True
     )
     app.add_transform(AutoStructify)
-   app.add_css_file('cyverse.css')
+    app.add_css_file('cyverse.css')
     app.add_css_file('detail-expand.css')
     app.add_css_file('question-answer.css')
     #uncomment to enable table sorting app.add_javascript('jquery.tablesorter.min.js')
